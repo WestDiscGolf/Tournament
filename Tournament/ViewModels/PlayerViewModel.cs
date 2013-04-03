e7b1804281f60a6ad50d2a331cc176cb5298a452
Adam Storr <git@icecubez.net>
2013-04-03 21:56:33 +0100

    Basic Player CRUD operations

---
+++ Tournament/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tournament.ViewModels
+{
+    public class PlayerViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Twitter { get; set; }
+        [Required]
+        public string Slug { get; set; }
+
+        public string FullName
+        {
+            get { return FirstName + " " + (string.IsNullOrWhiteSpace(NickName) ? "" : "\"" + NickName + "\" ") + LastName; }
+        }
+    }
+}
