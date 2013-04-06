919ef2dea728290a463bb815beea7046b6825a49
Adam Storr <git@icecubez.net>
2013-04-06 18:47:43 +0100

    Player and Team controller updates
    Update to mapping and select players by team

--- Tournament/Extensions.cs
+++ Tournament/Extensions.cs
@@ -4,9 +4,9 @@ namespace Tournament
 {
     public static class Extensions
     {
-        public static string Id(this string id)
+        public static string Id(this string ravenId)
         {
-            var split = id.Split('/');
+            var split = ravenId.Split('/');
             return (split.Count() == 2) ? split[1] : split[0];
         }
     }
