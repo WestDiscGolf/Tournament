1c082f6b557090d57bac15864ddfdc08186a1858
Adam Storr <git@icecubez.net>
2013-04-11 20:10:31 +0100

    Match CRUD drop #1

---
+++ Tournament/Entities/Comment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Tournament.Entities
+{
+    public class Comment
+    {
+        public DateTime DateTime { get; set; }
+        public string SubmittedBy { get; set; }
+        public string Content { get; set; }
+    }
+}
