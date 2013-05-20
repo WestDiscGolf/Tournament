91690e03797fe976ea0b8d3de90d17870ec5b5ec
Adam Storr <git@icecubez.net>
2013-05-20 18:38:38 +0100

    First attempt at a statistics index

--- Tournament/Controllers/HomeController.cs
+++ Tournament/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tournament.Infrastructure.Indexes;
 
 namespace Tournament.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            ViewBag.Stats = RavenSession.Query<Statistics_General.Result, Statistics_General>().FirstOrDefault() ?? new Statistics_General.Result();
 
             return View();
         }
---
+++ Tournament/Infrastructure/Indexes/Statistics_General.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using Raven.Client.Indexes;
+using Tournament.Entities;
+
+namespace Tournament.Infrastructure.Indexes
+{
+    public class Statistics_General : AbstractMultiMapIndexCreationTask<Statistics_General.Result>
+    {
+        public class Result
+        {
+            public int Events { get; set; }
+            public int Legs { get; set; }
+            public int Players { get; set; }
+            public int Matches { get; set; }
+            public int Extras { get; set; }
+        }
+
+        public Statistics_General()
+        {
+            // count the events
+            AddMap<Event>(events => from e in events
+                                    select new Result
+                                        {
+                                            Events = 1,
+                                            Extras = 0,
+                                            Legs = 0,
+                                            Matches = 0,
+                                            Players = 0
+                                        });
+
+            AddMap<Leg>(legs => from leg in legs
+                                    select new Result
+                                        {
+                                            Events = 0,
+                                            Extras = 0,
+                                            Legs = 1,
+                                            Matches = 0,
+                                            Players = 0
+                                        });
+
+            AddMap<Extra>(extras => from extra in extras
+                                    select new Result
+                                        {
+                                            Events = 0,
+                                            Extras = 0,
+                                            Legs = 1,
+                                            Matches = 0,
+                                            Players = 0
+                                        });
+
+            AddMap<Match>(matches => from match in matches
+                                    select new Result
+                                    {
+                                        Events = 0,
+                                        Extras = 0,
+                                        Legs = 0,
+                                        Matches = 1,
+                                        Players = 0
+                                    });
+
+            AddMap<Player>(players => from player in players
+                                    select new Result
+                                    {
+                                        Events = 0,
+                                        Extras = 0,
+                                        Legs = 0,
+                                        Matches = 0,
+                                        Players = 1
+                                    });
+
+            Reduce = statses => from result in statses
+                                group result by "contant"
+                                into g
+                                select new Result
+                                    {
+                                        Events = g.Sum(x => x.Events),
+                                        Extras = g.Sum(x => x.Extras),
+                                        Legs = g.Sum(x => x.Legs),
+                                        Matches = g.Sum(x => x.Matches),
+                                        Players = g.Sum(x => x.Players)
+                                    };
+        }
+    }
+}
