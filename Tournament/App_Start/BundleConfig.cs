60e2793f427570b000681b1ba3abb19005ebc00e
Adam Storr <git@icecubez.net>
2013-05-22 08:34:57 +0100

    Initial admin area styling
    First iteration of layout with bootstrap for the admin area

--- Tournament/App_Start/BundleConfig.cs
+++ Tournament/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Tournament
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/chosen.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(/*"~/Content/site.css",*/ "~/Content/chosen.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
